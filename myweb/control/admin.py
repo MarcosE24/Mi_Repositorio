5d514fbd14744208e85639bb99208e1592f8a17a
MarcosE <maedaes24@gmail.com>
2023-06-16 22:15:55 -0400

    se añadio la opcion de filtrado y ordenamiento de forma descendete, tambien que muestre todos los campos requeridos en las especificaciones

--- myweb/control/admin.py
+++ myweb/control/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from . import models
 
+class Empleado (admin.ModelAdmin):
+    ordering=["turno"]
+    list_display=[field.name for field in models.Empleado._meta.get_fields()]
+    list_filter=["nombre"]
+
+class Jornada (admin.ModelAdmin):
+    ordering=["fecha"]
+    list_display=[field.name for field in models.Jornada._meta.get_fields()]
+
 admin.site.register(models.Empleado)
 admin.site.register(models.Jornada)
