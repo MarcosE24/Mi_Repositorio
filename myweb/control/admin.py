26d8d81cf60b1a917067412f7f20f62247090655
MarcosE <maedaes24@gmail.com>
2023-06-16 22:26:53 -0400

    agregado los 2 empleados y 3 marcaciones de jornada

--- myweb/control/admin.py
+++ myweb/control/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from . import models
 
 class Empleado (admin.ModelAdmin):
-    ordering=["turno"]
+    ordering=["-turno"]
     list_display=[field.name for field in models.Empleado._meta.get_fields()]
     list_filter=["nombre"]
 
 class Jornada (admin.ModelAdmin):
-    ordering=["fecha"]
+    ordering=["-fecha"]
     list_display=[field.name for field in models.Jornada._meta.get_fields()]
 
 admin.site.register(models.Empleado)
