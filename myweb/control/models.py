786e337ef226b7d1aa68a92dd3933e2259e54b4b
MarcosE <maedaes24@gmail.com>
2023-06-16 21:54:23 -0400

    creacion de las entidades e instalacion de la app en el admin

--- myweb/control/models.py
+++ myweb/control/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Empleado (models.Model):
+    turnos=(("opcion1","Mañana"),
+              ("opcion2","Tarde"),
+              ("opcion3","Noche"),
+    )
+    nombre=models.CharField(max_length=150, blank=False)
+    departamento=models.CharField(max_length=100, blank=False)
+    fecha_inicio=models.DateField(auto_now_add=True, blank=False)
+    dias_trabajo=models.CharField(max_length=100, blank=False)
+    turno=models.CharField(blank=False, choices=turnos, max_length=50)
+    hora_entrada=models.TimeField(blank=False)
+    hora_salida=models.TimeField(blank=False)
+
+class Jornada (models.Model):
+    marcacion=(("opcion1","Entrada"),
+              ("opcion2","Salida"),
+    )
+    id_empleado=models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    fecha=models.DateTimeField(blank=False)
+    tipo_marcacion=models.CharField(blank=False, choices=marcacion, max_length=50)
+
